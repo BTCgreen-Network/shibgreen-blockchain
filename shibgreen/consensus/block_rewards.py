f4fdfd35e3cf452f675cf2897e135a372abae58e
BTCgreen-Network <svginsomnia@gmail.com>
2021-11-09 07:24:49 -0500

    Blocks

--- shibgreen/consensus/block_rewards.py
+++ shibgreen/consensus/block_rewards.py
@@ -16,11 +16,11 @@ def calculate_pool_reward(height: uint32) -> uint64:
 
     if height == 0:
         return uint64(int((7 / 8) * 100000000 * _byte_per_shibgreen))
-    elif height < 0.5 * _blocks_per_year:
+    elif height < 0.1 * _blocks_per_year:
         return uint64(int((7 / 8) * 100000 * _byte_per_shibgreen))
-    elif height < 1.5 * _blocks_per_year:
+    elif height < 0.5 * _blocks_per_year:
         return uint64(int((7 / 8) * 20000 * _byte_per_shibgreen))
-    elif height < 3 * _blocks_per_year:
+    elif height < 1 * _blocks_per_year:
         return uint64(int((7 / 8) * 7500 * _byte_per_shibgreen))
     elif height < 6 * _blocks_per_year:
         return uint64(int((7 / 8) * 2000 * _byte_per_shibgreen))
@@ -39,11 +39,11 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     """
     if height == 0:
         return uint64(int((1 / 8) * 100000000 * _byte_per_shibgreen))
-    elif height < 0.5 * _blocks_per_year:
+    elif height < 0.1 * _blocks_per_year:
         return uint64(int((1 / 8) * 100000 * _byte_per_shibgreen))
-    elif height < 1.5 * _blocks_per_year:
+    elif height < 0.5 * _blocks_per_year:
         return uint64(int((1 / 8) * 20000 * _byte_per_shibgreen))
-    elif height < 3 * _blocks_per_year:
+    elif height < 1 * _blocks_per_year:
         return uint64(int((1 / 8) * 7500 * _byte_per_shibgreen))
     elif height < 6 * _blocks_per_year:
         return uint64(int((1 / 8) * 2000 * _byte_per_shibgreen))
